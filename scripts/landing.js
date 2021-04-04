face10d8912c37dc8b9cf1ee36f21d28ff3a4dea
Ash-Bhal <77412117+Ash-Bhal@users.noreply.github.com>
2021-04-03 22:05:29 -0700

    Add files via upload

---
+++ scripts/landing.js
@@ -0,0 +1,36 @@
+const goButton = document.querySelector('#go-btn');
+const demoButton = document.querySelector('#demo-btn');
+const textField = document.querySelector('#text-field');
+
+function getVideoId(url) {
+    const parts = url.split('v=');
+    if (parts.length < 2) {
+        return '';
+    }
+    var videoId = parts[1];
+    var ampersandPosition = videoId.indexOf('&');
+    if (ampersandPosition != -1) {
+        videoId = videoId.substring(0, ampersandPosition);
+    }
+    return videoId;
+}
+
+goButton.addEventListener('click', function (event) {
+    const videoId = getVideoId(textField.value);
+    location.href = '/notes?vid=' + videoId;
+});
+
+demoButton.addEventListener('click', function(event) {
+    textField.value = 'https://www.youtube.com/watch?v=p_o4aY7xkXg';
+    updateButton();
+});
+
+function updateButton() {
+    const id = getVideoId(textField.value);
+    goButton.disabled = id.length == 0;
+}
+
+textField.addEventListener('input', updateButton);
+textField.addEventListener('propertychange', updateButton);
+
+updateButton();
---
+++ scripts/notes.js
@@ -0,0 +1,83 @@
+const spinner = document.querySelector('#spinner');
+const animContainer = document.querySelector('#anim-container');
+
+const title = document.querySelector('#title');
+const creatorName = document.querySelector('#creator-name');
+const watchButton = document.querySelector('#watch-btn');
+const textContainer = document.querySelector('#text-container');
+const exportButton = document.querySelector('#export-btn');
+
+var isTesting = false;
+
+function hideAnimation() {
+    animContainer.style.transition = 'all 0.3s';
+    animContainer.style.opacity = '0';
+    setTimeout(() => {
+        animContainer.style.display = 'none';
+    }, 300);
+}
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function getNotes() {
+    const videoId = getParameterByName('vid');
+    if (!(typeof videoId === 'string' && videoId.length > 0)) {
+        location.href = '/';
+        return;
+    }
+    const url =  isTesting ? 'https://lecture-noted-backend.herokuapp.com/testingv2' : 'https://lecture-noted-backend.herokuapp.com/notes/' + videoId;
+    const opts = {
+        method: 'GET',
+    }
+    fetch(url, opts).then(function(res) {
+        return res.json();
+    })
+    .then(function(result) {
+        title.innerHTML = result.metadata.title;
+        creatorName.innerHTML = result.metadata.author;
+        watchButton.href = 'https://youtube.com/watch?v=' + videoId;
+        var addedImage = false;
+        result.response.forEach((obj) => {
+            if (obj.type === 'text') {
+                const bullet = document.createElement('div');
+                bullet.innerHTML = obj.data;
+                bullet.classList.add('Notes-bullet');
+                if ('time' in obj) {
+                    const link = document.createElement('a');
+                    const time = Math.round(obj.time);
+                    var seconds = time % 60;
+                    seconds = (seconds < 10) ? `0${seconds}` : `${seconds}`;
+                    const minutes = Math.trunc(time / 60);
+                    link.innerHTML = `[${minutes}:${seconds}]`;
+                    link.href = `https://youtu.be/${videoId}?t=${time}`;
+                    link.classList.add('Notes-intextLink');
+                    bullet.insertBefore(link, bullet.firstChild);
+                }
+                textContainer.appendChild(bullet);
+            } 
+            else if (obj.type === 'image' && !addedImage) {
+                const image = document.createElement('img');
+                image.src = obj.data.image;
+                image.classList.add('Notes-image');
+                textContainer.insertBefore(image, textContainer.firstChild)
+                addedImage = true;
+            }
+        });
+        result.metadata.videoId = videoId;
+        const origData = JSON.stringify(result);
+        exportButton.href = `https://lecture-noted-backend.herokuapp.com/docx?data=${origData}`;
+        hideAnimation();
+    })
+    .catch(function(error) {
+        // location.href = '/';
+    });
+}
+
+getNotes();
